846c1c4d0f293359c981f369b439b2241d65882d
Seth Theeke <seththeeke@gmail.com>
2023-02-05 16:34:42 -0600

    isUnique and checkPermutations

---
+++ arrays-and-strings/IsUnique.java
@@ -0,0 +1,45 @@
+// Implement an algorithm to determine if a string has all unique characters.
+
+/*
+
+This solution can be done in O(n) worst case time and O(n) worst case space using a Set to collect all characters.
+For each character, add it to the set, if the character already exists, then return false.
+
+*/
+
+// Space => O(n) where n is the length of the input
+// Time => O(n) where n is the length of the input
+public boolean isUnique(String input) {
+    Set<Character> charSet = new Set<>();
+    for (int i = 0; i < input.length; i++) {
+        if (charSet.contains(input.charAt(i))) {
+            return false;
+        }
+    }
+    return true;
+}
+
+//  What if you cannot use additional data structures?
+
+/*
+
+If no additional data structures could be used, then you can instead sort the string's characters in the string itself.
+After that, you can check all the characters in the string and check i and i + 1. If at any point i==i+1, then you know
+the string does not contain all unique characters. However, depending on the language, this may require an additional 
+data structure to solve Another approach would be to use indexOf with substrings of Strings given the character you are 
+checking. If the character you are reading exists in the substring of the remaining characters then it is not all unique.
+
+*/
+
+// Space => O(n) where n is the length of the input - 1 because we are creating a new substring on each pass
+// Time => O(n) where n is the length of the input
+public boolean isUnique(String input) {
+    for (int i = 0; i < input.length - 1; i++) {
+        char currChar = input[i];
+        String substring = input.substring(i+1);
+        if (substring.indexOf(currChar) > -1) {
+            return false;
+        }
+    }
+    return true;
+}
---
+++ arrays-and-strings/checkPermutations.java
@@ -0,0 +1,70 @@
+// Given two strings, write a method to determine if one is a permutation of the other
+
+/*
+
+Assuming a permutation is simple the same characters in both strings, then one approach is to sort the strings
+and then check if they are equal. 
+
+*/
+
+// Space => O(n) where n is the length of the two strings
+// Time => O(nlogn) where n is the length of the two strings. This is because of the quicksort from Arrays.sort
+public boolean checkPermutations(String one, String two) {
+    if (one.length != two.length) {
+        return false;
+    }
+
+    return sortString(one).equalsIgnoreCase(sortString(two));
+}
+
+private String sortString(String str) {
+    char[] characterArr = str.toCharArray();
+
+    Arrays.sort(characterArr);
+
+    return characterArr.toString();
+}
+
+/*
+
+Another approach would be to use a map and do a simple character count. You
+can then verify the permutation by going through all entries in the map and ensuring all the values are the same.
+
+*/
+
+// Space => O(n) where n is the length of the strings. It comes from the creation of the map objects
+// Time => O(n) where n is the length of the strings. This is from building the map from all characters
+public boolean checkPermutations(String one, String two) {
+    if (one.length != two.length) {
+        return false;
+    }
+
+    Map<Character, Integer> charCountOne = getCharCountForString(one);
+    Map<Character, Integer> charCountTwo = getCharCountForString(two);
+
+    if (charCountOne.getKeys().length != charCountTwo.getKeys().length) {
+        return false;
+    }
+
+    for (Entry<Character, Integer> entry : charCountOne) {
+        Character key = entry.getKey();
+        if (charCountTwo.get(key) != entry.getValue()) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+private Map<Character, Integer> getCharCountForString(String str) {
+    Map<Character, Integer> charCount = new HashMap<>();
+    for (int i = 0; i < str.length; i++) {
+        Character currChar = str.charAt(i);
+        if (!charCount.containsKey(currChar)) {
+            charCount.put(currChar, 0);
+        }
+        charCount.put(currChat, charCount.get(currChar) + 1);
+    }
+
+    return charCount;
+}
